244b36e9563f29f0258d93664646b0b0a31b2b21
Keks <keks@htmlacademy.ru>
2022-05-09 17:56:31 +0300

    :heavy_check_mark: Сборка #8

--- 8/js/script.js
+++ 8/js/script.js
@@ -1,16 +1,1 @@
-let navMain = document.querySelector('.lead-navigation');
-let navToggle = document.querySelector('.lead-navigation__toggle');
-
-if (navMain.classList.contains('lead-navigation--nojs')) {
-    navMain.classList.remove('lead-navigation--nojs');
-}
-
-navToggle.addEventListener('click', function () {
-    if (navMain.classList.contains('lead-navigation--closed')) {
-        navMain.classList.remove('lead-navigation--closed');
-        navMain.classList.add('lead-navigation--opened');
-    } else {
-        navMain.classList.add('lead-navigation--closed');
-        navMain.classList.remove('lead-navigation--opened');
-    }
-});
+let navMain=document.querySelector(".lead-navigation"),navToggle=document.querySelector(".lead-navigation__toggle");navMain.classList.contains("lead-navigation--nojs")&&navMain.classList.remove("lead-navigation--nojs"),navToggle.addEventListener("click",(function(){navMain.classList.contains("lead-navigation--closed")?(navMain.classList.remove("lead-navigation--closed"),navMain.classList.add("lead-navigation--opened")):(navMain.classList.add("lead-navigation--closed"),navMain.classList.remove("lead-navigation--opened"))}));
